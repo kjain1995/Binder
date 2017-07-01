19132da3c42c0a8cd7c426c4221a50fb11c72741
als26 <abilas96@gmail.com>
2017-07-01 17:18:11 -0400

    adding flexbox layout

--- js/custom.js
+++ js/custom.js
@@ -2,13 +2,13 @@ function openTab(evt, tabName) {
     // Declare all variables
     var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
+    // Hide all tab content
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    // Get all elements with class="tablinks" and remove the class "active"
+    // Set all tabs to unfocused
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
